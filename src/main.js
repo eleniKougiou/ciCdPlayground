1eb4733a4e65c0a7f967ba9daaa904d77d3f321f
Eleni Kougioumtzi <eleni.kougioumtzi@tngtech.com>
2025-05-16 10:45:06 +0200

    Update with personal info

--- src/main.js
+++ src/main.js
@@ -3,16 +3,12 @@ import App from "./App.svelte";
 new App({
   target: document.body,
   props: {
-    // What's your name?
-    name: "James Butler",
-    // In the following fiels you can either give a single string,
-    // or an array of bullet points
-
-    // What do you associate with the term 'CI/CD'?
-    associations: ["Ehm, what?"],
-    // Which CI/CD tools do you use in your project?
-    tools: "Jenkins, maybe?",
-    // What do you want to learn in this workshop?
-    expectations: ["what you are talking about"],
+    name: "Eleni",
+    // Associations with the term 'CI/CD'
+    associations: ["Integration", "Deployment", "Automation"],
+    // CI/CD tools in the client project
+    tools: "Jenkins",
+    // Workshop expectations
+    expectations: ["overview of Github Actions", "better understanding of Jenkins"],
   },
 });
